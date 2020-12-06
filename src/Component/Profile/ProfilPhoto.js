da6e15898efdc53cb00b3598959ddff41de691f6
walda8 <74023511+walda8@users.noreply.github.com>
2020-12-06 19:53:43 +0100

    Update ProfilPhoto.js

--- src/Component/Profile/ProfilPhoto.js
+++ src/Component/Profile/ProfilPhoto.js
@@ -2,9 +2,9 @@ import myPic from './profilepic.jpg'
 import React from 'react';
 const MyPic = () => {
     return ( 
-        
+     <>   
         <img src={myPic} className="me" alt ='myImage'/>
-
+     </>
         )
 }
 export default MyPic;
